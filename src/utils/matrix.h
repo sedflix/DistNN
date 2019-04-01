d464a6fdc565b4028a3d6b776388abe360a225ef
geekSiddharth <siddharth16268@iiitd.ac.in>
2019-04-02 01:08:46 +0530

    removed helper dependency

--- src/utils/matrix.h
+++ src/utils/matrix.h
@@ -4,7 +4,17 @@
 
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
-#include "helper_cuda.h"
+
+// citation: https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+#define checkCudaErrors(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
 
 
 
